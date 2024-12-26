0e427c0dc8a0f22d8b79be48af057c018196ef03
juan miguel <Miguel.Rojas@jala.university>
2024-12-25 23:31:02 -0500

    add setters and getters in the client class

--- src/main/java/gym_fit/domain/Client.java
+++ src/main/java/gym_fit/domain/Client.java
@@ -23,4 +23,36 @@ public class Client {
         this(name, lastName, membership);
         this.membership = membership;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMembership() {
+        return membership;
+    }
+
+    public void setMembership(int membership) {
+        this.membership = membership;
+    }
 }
