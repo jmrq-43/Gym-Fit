b9698dcbf438fb18c1277f89bb1044354e317648
juan miguel <Miguel.Rojas@jala.university>
2024-12-26 01:03:22 -0500

    configuring the ClientList method of the ClientDAO class

--- src/main/java/gym_fit/data/ClientDAO.java
+++ src/main/java/gym_fit/data/ClientDAO.java
@@ -2,7 +2,13 @@ package gym_fit.data;
 
 import gym_fit.domain.Client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+
+import static gym_fit.connection.ConnectionDb.getConnection;
 
 public class ClientDAO implements IClientDAO {
     @Override
@@ -27,6 +33,33 @@ public class ClientDAO implements IClientDAO {
 
     @Override
     public List<Client> clientList() {
-        return List.of();
+        List<Client> clientList = new ArrayList<>();
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        Connection connectionDb = getConnection();
+
+        var sql = "SELECT * FROM  client ORDER BY id ";
+
+        try {
+            preparedStatement = connectionDb.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                var client = new Client();
+                client.setId(resultSet.getInt("id"));
+                client.setName(resultSet.getString("name"));
+                client.setLastName(resultSet.getString("lastName"));
+                client.setMembership(resultSet.getInt("membership"));
+                clientList.add(client);
+            }
+        } catch (Exception e) {
+            System.out.println("error to list clients " + e.getMessage());
+        } finally {
+            try {
+                connectionDb.close();
+            } catch (Exception e) {
+                System.out.println("error closing connection " + e.getMessage());
+            }
+        }
+        return clientList;
     }
 }
