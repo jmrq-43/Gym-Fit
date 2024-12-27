8162e094c80fd227ba9f292d1b5e28e7634373f2
juan miguel <Miguel.Rojas@jala.university>
2024-12-26 19:18:13 -0500

    printMenu and executeOptions were added whithout any functionality

--- src/main/java/gym_fit/presentation/GymFitAppMenu.java
+++ src/main/java/gym_fit/presentation/GymFitAppMenu.java
@@ -1,5 +1,32 @@
 package gym_fit.presentation;
 
-public class GymFitAppMenu {
+import gym_fit.data.ClientDAO;
 
+import java.util.Scanner;
+
+public class GymFitAppMenu {
+    public static void menu() {
+        var leave = false;
+        var console = new Scanner(System.in);
+        var clientDAO = new ClientDAO();
+        while (!leave) {
+            try {
+                printMenu();
+                leave = executeOptions(console, clientDAO);
+
+            } catch (Exception e) {
+                System.out.println("Error to execute menu: " + e.getMessage());
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean executeOptions(Scanner console, ClientDAO clientDAO) {
+
+        return false;
+    }
+
+    private static void printMenu() {
+
+    }
 }
