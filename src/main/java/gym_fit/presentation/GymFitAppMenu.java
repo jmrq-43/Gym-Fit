62b445e6c86aa772b8687df03e4dc8a88881c1b2
juan miguel <Miguel.Rojas@jala.university>
2024-12-26 19:42:27 -0500

    added case 1 of the executeOptions method

--- src/main/java/gym_fit/presentation/GymFitAppMenu.java
+++ src/main/java/gym_fit/presentation/GymFitAppMenu.java
@@ -1,6 +1,7 @@
 package gym_fit.presentation;
 
 import gym_fit.data.ClientDAO;
+import gym_fit.data.IClientDAO;
 
 import java.util.Scanner;
 
@@ -8,7 +9,7 @@ public class GymFitAppMenu {
     public static void menu() {
         var leave = false;
         var console = new Scanner(System.in);
-        var clientDAO = new ClientDAO();
+        IClientDAO clientDAO = new ClientDAO();
         while (!leave) {
             try {
                 var option = printMenu(console);
@@ -21,9 +22,17 @@ public class GymFitAppMenu {
         }
     }
 
-    private static boolean executeOptions(Scanner console, ClientDAO clientDAO, int option) {
-
+    private static boolean executeOptions(Scanner console, IClientDAO clientDAO, int option) {
+        var leave = false;
+        switch (option){
+            case 1 -> listClients(clientDAO);
+        }
         return false;
+    }
+
+    private static void listClients(IClientDAO clientDAO) {
+        var clients = clientDAO.clientList();
+        clients.forEach(System.out::println);
     }
 
     private static int printMenu(Scanner console) {
